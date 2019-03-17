facb210d0de686d20f054c149494bdf2d679a4db
yopaseopor <yopaseopor@gmail.com>
2019-03-17 15:56:05 +0100

    Update layerdef.js

--- js_source/layerdef.js
+++ js_source/layerdef.js
@@ -274,11 +274,7 @@ function popuplinks(lonlat){
 	  thelink = thelink + "<a href=\"http://www.bing.com/maps/?v=2&cp=" + lonlat.lat + "~" + lonlat.lon + "&lvl=17&dir=0&sty=h&form=LMLTCC\" target=\"_blank\"><img src='img/bing.gif'>Bing</a><p><hr>";
 	  thelink = thelink + "<a href=\"https://wego.here.com/?map="  + (lonlat.lat) + "," + (lonlat.lon ) + "17,normal"  + "\" target=\"_blank\"><img src='img/openstreetcam.png'>OpenStreetCam</a><hr>";
 	  thelink = thelink + "<b>Satelitales :</b><br><a href=\"https://wego.here.com/?map="  + (lonlat.lat) + "," + (lonlat.lon ) + "17,satellite"  + "\" target=\"_blank\"><img src='img/openstreetcam.png'>DigitalGlobe</a><hr>";
-	thelink = thelink + "<a href=\"https://maps.google.es/maps?ll=" + lonlat.lat + "," + lonlat.lon + ",602m/data=!3m1!1e3\" target=\"_blank\"><img src='img/google.gif'>Google SAT</a>&nbsp&nbsp";
-	  thelink = thelink + "<b>A pie de calle :</b><br><a href=\"https://www.mapillary.com/app/?lat="  + (lonlat.lat) + "&lng=" + (lonlat.lon ) + "&z=15"  + "\" target=\"_blank\"><img src='img/mapillary.png'>Mapillary</a>";
-	  thelink = thelink + "<a href=\"https://openstreetcam.org/map/@"  + (lonlat.lat) + "," + (lonlat.lon ) + "17z"  + "\" target=\"_blank\"><img src='img/openstreetcam.png'>OpenStreetCam</a><hr>";
-	  thelink = thelink + "<b>Recorridos :</b><br><a href=\"http://cycling.waymarkedtrails.org/nl/?zoom=13" +  "&lat=" + lonlat.lat + "&lon=" + lonlat.lon + "\" target=\"_blank\"><img src='img/lonvia.gif'>Waymarked trails</a><hr>";
-	  thelink = thelink + "<a href=\"https://www.wikiloc.com/wikiloc/map.do?lt=" + lonlat.lat + "&ln=" + lonlat.lon + "&z=17" target=\"_blank\"><img src='img/lonvia.gif'>Wikiloc</a><hr>";
+	
 	  var area = 0.01
 	  var ctop = lonlat.lat + area;
 	  var cbottom = ctop - (2 * area);
